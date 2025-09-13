3b35c4933b6c8f060d25f57c2eda912731639d38
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-09-13 17:08:02 +0200

    func getLinesReader

--- src/main/java/com/jxvxxrbc/httpwebserver/App.java
+++ src/main/java/com/jxvxxrbc/httpwebserver/App.java
@@ -1,9 +1,15 @@
 package com.jxvxxrbc.httpwebserver;
 
+import java.io.FileInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App {
+
+	String rutaArchivo = "src/main/java/com/jxvxxrbc/httpwebserver/messages.txt";
+	byte[] buffer = new byte[8];
+	StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
 		// server socket to handle connections
 		try (ServerSocket server = new ServerSocket(1234)) {
@@ -16,49 +22,28 @@ public class App {
 		} catch (Exception e) {
 			System.out.println("error " + e.getMessage());
 		}
-
-		//// ruta del archivo
-		// String rutaArchivo = "src/main/java/com/jxvxxrbc/httpwebserver/messages.txt";
-
-		//// tamaño del buffer a leer
-		// byte[] buffer = new byte[8];
-
-		//// StringBuilder para almacenar los caracteres
-		// StringBuilder sb = new StringBuilder();
-
-		//// leer el archivo
-		// try (FileInputStream fis = new FileInputStream(rutaArchivo)) {
-		// int readBytes;
-
-		// // leemos hasta eof
-		// while ((readBytes = fis.read(buffer)) != -1) {
-		// // recorremos los bytes leidos
-		// for (int i = 0; i < readBytes; i++) {
-		// // guardamos los caracteres del buffer por indice
-		// char c = (char) buffer[i];
-		// // Si uno de los caracteres es un salto de linea
-		// if (c == '\n') {
-		// // imprimimos a consola
-		// System.out.printf("read: %s\n", sb.toString());
-		// // limpiar StringBuilder
-		// sb.setLength(0);
-		// } else {
-		// // si no es salto de linea lo añadimos al sb
-		// sb.append(c);
-		// }
-		// }
-		// }
-
-		// // imprimir la ultima linea (no \n)
-		// if (sb.length() > 0) {
-		// System.out.printf("read: %s\n", sb.toString());
-		// }
-
-		// } catch (IOException e) {
-		// System.err.println("error al leer el archivo " + e.getMessage());
-		// }
 	}
 
-	public void getLinesReader() {
+	public void getLinesReader(Socket s) {
+		int readBytes;
+		try (FileInputStream fis = new FileInputStream(rutaArchivo)) {
+			while ((readBytes = fis.read(buffer)) != -1) {
+				for (int i = 0; i < readBytes; i++) {
+					char c = (char) buffer[i];
+					if (c == '\n') {
+						System.out.printf("read: %s\n", sb.toString());
+						sb.setLength(0);
+					} else {
+						sb.append(c);
+					}
+				}
+
+				if (sb.length() > 0) {
+					System.out.printf("%s\n", sb.toString());
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("error: " + e.getMessage());
+		}
 	}
 }
