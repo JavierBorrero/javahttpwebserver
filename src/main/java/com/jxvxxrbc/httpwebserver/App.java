185c903389963ab649ed8a775d43ad3a0b17faec
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-09-15 15:28:27 +0200

    reading messages from nc command

--- src/main/java/com/jxvxxrbc/httpwebserver/App.java
+++ src/main/java/com/jxvxxrbc/httpwebserver/App.java
@@ -1,14 +1,13 @@
 package com.jxvxxrbc.httpwebserver;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App {
 
-	String rutaArchivo = "src/main/java/com/jxvxxrbc/httpwebserver/messages.txt";
-	byte[] buffer = new byte[8];
-	StringBuilder sb = new StringBuilder();
+	static String rutaArchivo = "src/main/java/com/jxvxxrbc/httpwebserver/messages.txt";
+	static byte[] buffer = new byte[1024];
 
 	public static void main(String[] args) {
 		// server socket to handle connections
@@ -16,34 +15,40 @@ public class App {
 			System.out.println("server iniciado");
 
 			// accept connections and print inetAddress
-			Socket client = server.accept();
-			System.out.println("client: " + client.getInetAddress());
+			Socket conn = server.accept();
+			System.out.println("client: " + conn.getInetAddress());
+
+			String s = getLinesReader(conn);
+			System.out.println("read: " + s);
+
+			conn.close();
+			server.close();
 
 		} catch (Exception e) {
 			System.out.println("error " + e.getMessage());
 		}
 	}
 
-	public void getLinesReader(Socket s) {
+	public static String getLinesReader(Socket s) {
+		String str = "";
 		int readBytes;
-		try (FileInputStream fis = new FileInputStream(rutaArchivo)) {
-			while ((readBytes = fis.read(buffer)) != -1) {
+
+		try {
+			InputStream is = s.getInputStream();
+			while ((readBytes = is.read(buffer)) != -1) {
 				for (int i = 0; i < readBytes; i++) {
 					char c = (char) buffer[i];
 					if (c == '\n') {
-						System.out.printf("read: %s\n", sb.toString());
-						sb.setLength(0);
+						return str;
 					} else {
-						sb.append(c);
+						str += c;
 					}
-				}
-
-				if (sb.length() > 0) {
-					System.out.printf("%s\n", sb.toString());
 				}
 			}
 		} catch (Exception e) {
 			System.out.println("error: " + e.getMessage());
 		}
+
+		return str;
 	}
 }
